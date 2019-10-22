65e2294832a2fbccfcfa48202cf4989d1f3981ad
MarioMorales7x7 <mario.morales.texas@gmail.com>
2019-10-22 09:42:21 -0500

    added dependancies

---
+++ server/server.js
@@ -0,0 +1,12 @@
+  
+const express = require('express');
+const bodyParser = require('body-parser');
+const axios = require('axios');
+const PORT = 3333;
+
+const app = express();
+
+app.use(express.static(__dirname + '/../public'));
+app.use(bodyParser.json());
+
+app.listen(PORT, () => console.log(`proxy is listening on ${PORT}`));
